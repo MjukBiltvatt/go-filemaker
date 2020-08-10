2f57935744d98af72eeae9824ede121f1592c90b
jomla97 <jomla97@gmail.com>
2020-08-10 12:46:57 +0200

    fix export package types

--- exportPackageTypes.go
+++ exportPackageTypes.go
@@ -6,10 +6,10 @@ import (
 )
 
 //Record represents the record object returned by performed findcommands
-type Record record.Record
+type Record = record.Record
 
 //FieldChange represents the object returned by record.SetField()
-type FieldChange record.FieldChange
+type FieldChange = record.FieldChange
 
 //Connection represents the connection object returned by filemaker.Connect()
-type Connection connection.Connection
+type Connection = connection.Connection
