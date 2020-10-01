a10d4105d21fca407790298671094516801d54ff
jomla97 <jomla97@gmail.com>
2020-10-02 01:06:44 +0200

    Resume()

---
+++ resume.go
@@ -0,0 +1,10 @@
+package filemaker
+
+import (
+	"github.com/jomla97/go-filemaker/pkg/session"
+)
+
+//Resume resumes a database session with the specified token
+func Resume(host string, database string, username string, password string, token string) (*session.Session, error) {
+	return session.Resume(host, database, username, password, token)
+}
